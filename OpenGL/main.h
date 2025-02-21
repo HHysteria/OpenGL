2b91454af957fc8c06ea61e0a7822a92c66696d0
User <User@DESKTOP-H2MR96A>
2025-02-21 03:41:36 -0500

    Add project files.

---
+++ OpenGL/main.h
@@ -0,0 +1,14 @@
+#ifndef __OPENGL_MAIN_H__
+#define __OPENGL_MAIN_H__
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+
+
+#include <iostream>
+
+
+using namespace std;
+
+#endif // __OPENGL_MAIN_H__
---
+++ OpenGL/player.h
@@ -0,0 +1,30 @@
+#ifndef __OPENGL_PLAYER_H__
+#define __OPENGL_PLAYER_H__
+
+
+#include <GLFW/glfw3.h>
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+class player
+{
+private:
+	
+
+
+
+public:
+	player();
+
+};
+
+
+
+
+#endif // __OPENGL_MAIN_H__
